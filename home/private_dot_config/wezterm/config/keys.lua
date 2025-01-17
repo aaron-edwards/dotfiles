7378a153e9abad0b0c3e8d2dd05c743ce066592d
Aaron Edwards <edwards.aaronmark@gmail.com>
2025-01-17 20:38:39 +1100

    [wezterm] Changes keybindings to play nicer with vim

--- home/private_dot_config/wezterm/config/keys.lua
+++ home/private_dot_config/wezterm/config/keys.lua
@@ -2,11 +2,11 @@ local M = {}
 
 function M.apply(config, wezterm)
 	config.keys = {
-		{ key = "\\", mods = "CTRL", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "-", mods = "CTRL", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "t", mods = "CTRL", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-		{ key = "w", mods = "CTRL", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-		{ key = "l", mods = "CTRL", action = wezterm.action({ EmitEvent = "toggle-dark-mode" }) },
+		{ key = "\\", mods = "CTRL|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "-", mods = "CTRL|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "t", mods = "CTRL|SHIFT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "w", mods = "CTRL|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+		{ key = "s", mods = "CTRL|SHIFT", action = wezterm.action({ EmitEvent = "toggle-dark-mode" }) },
 	}
 	return config
 end
