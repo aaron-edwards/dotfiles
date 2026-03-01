75dafdc0ab4ba22d7faf682fd3542a61b5e07bc4
Aaron Edwards <edwards.aaronmark@gmail.com>
2026-03-01 21:56:09 +1100

    More neovim setup

--- home/private_dot_config/nvim/lua/config/options.lua
+++ home/private_dot_config/nvim/lua/config/options.lua
@@ -18,9 +18,7 @@ opt.number = true           -- Show line number
 opt.showmatch = true        -- Highlight matching parenthesis
 opt.termguicolors = true    -- Enable 24-bit RGB colors
 --opt.signcolumn = "yes"
---vim.o.list = true           -- Show leading spaces and other "hidden" chars
-vim.opt.listchars:append("lead:⋅,extends:…,precedes:…,nbsp:␣")
-vim.o.background="light"
+vim.o.background = "light"
 
 -----------------------------------------------------------
 -- Tabs, indent
@@ -33,9 +31,7 @@ opt.smartindent = true      -- Autoindent new lines
 -----------------------------------------------------------
 -- Memory, CPU
 -----------------------------------------------------------
-opt.hidden = true           -- Enable background buffers
 opt.history = 100           -- Remember N lines in history
-opt.lazyredraw = true       -- Faster scrolling
 opt.synmaxcol = 240         -- Max column for syntax highlight
 opt.updatetime = 250        -- ms to wait for trigger an event
 
